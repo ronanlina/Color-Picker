5dc305a80fce0d0f8e30569c0da6f5507a72a00f
ronan <ronanlina@gmail.com>
2018-10-09 22:42:40 +0800

    initial commit

---
+++ ColorPicker/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ColorPicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdbRed = new System.Windows.Forms.RadioButton();
+            this.rdbBlue = new System.Windows.Forms.RadioButton();
+            this.rdbGreen = new System.Windows.Forms.RadioButton();
+            this.rdbYellow = new System.Windows.Forms.RadioButton();
+            this.btnPick = new System.Windows.Forms.Button();
+            this.pbxCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdbRed
+            // 
+            this.rdbRed.AutoSize = true;
+            this.rdbRed.Location = new System.Drawing.Point(12, 50);
+            this.rdbRed.Name = "rdbRed";
+            this.rdbRed.Size = new System.Drawing.Size(55, 21);
+            this.rdbRed.TabIndex = 0;
+            this.rdbRed.TabStop = true;
+            this.rdbRed.Text = "Red";
+            this.rdbRed.UseVisualStyleBackColor = true;
+            // 
+            // rdbBlue
+            // 
+            this.rdbBlue.AutoSize = true;
+            this.rdbBlue.Location = new System.Drawing.Point(12, 90);
+            this.rdbBlue.Name = "rdbBlue";
+            this.rdbBlue.Size = new System.Drawing.Size(57, 21);
+            this.rdbBlue.TabIndex = 1;
+            this.rdbBlue.TabStop = true;
+            this.rdbBlue.Text = "Blue";
+            this.rdbBlue.UseVisualStyleBackColor = true;
+            // 
+            // rdbGreen
+            // 
+            this.rdbGreen.AutoSize = true;
+            this.rdbGreen.Location = new System.Drawing.Point(12, 132);
+            this.rdbGreen.Name = "rdbGreen";
+            this.rdbGreen.Size = new System.Drawing.Size(69, 21);
+            this.rdbGreen.TabIndex = 2;
+            this.rdbGreen.TabStop = true;
+            this.rdbGreen.Text = "Green";
+            this.rdbGreen.UseVisualStyleBackColor = true;
+            // 
+            // rdbYellow
+            // 
+            this.rdbYellow.AutoSize = true;
+            this.rdbYellow.Location = new System.Drawing.Point(12, 176);
+            this.rdbYellow.Name = "rdbYellow";
+            this.rdbYellow.Size = new System.Drawing.Size(69, 21);
+            this.rdbYellow.TabIndex = 3;
+            this.rdbYellow.TabStop = true;
+            this.rdbYellow.Text = "Yellow";
+            this.rdbYellow.UseVisualStyleBackColor = true;
+            // 
+            // btnPick
+            // 
+            this.btnPick.Location = new System.Drawing.Point(12, 283);
+            this.btnPick.Name = "btnPick";
+            this.btnPick.Size = new System.Drawing.Size(244, 46);
+            this.btnPick.TabIndex = 4;
+            this.btnPick.Text = "Pick";
+            this.btnPick.UseVisualStyleBackColor = true;
+            this.btnPick.Click += new System.EventHandler(this.btnPick_Click);
+            // 
+            // pbxCanvas
+            // 
+            this.pbxCanvas.Location = new System.Drawing.Point(104, 50);
+            this.pbxCanvas.Name = "pbxCanvas";
+            this.pbxCanvas.Size = new System.Drawing.Size(140, 147);
+            this.pbxCanvas.TabIndex = 5;
+            this.pbxCanvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 341);
+            this.Controls.Add(this.pbxCanvas);
+            this.Controls.Add(this.btnPick);
+            this.Controls.Add(this.rdbYellow);
+            this.Controls.Add(this.rdbGreen);
+            this.Controls.Add(this.rdbBlue);
+            this.Controls.Add(this.rdbRed);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Color Picker";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbRed;
+        private System.Windows.Forms.RadioButton rdbBlue;
+        private System.Windows.Forms.RadioButton rdbGreen;
+        private System.Windows.Forms.RadioButton rdbYellow;
+        private System.Windows.Forms.Button btnPick;
+        private System.Windows.Forms.PictureBox pbxCanvas;
+    }
+}
+
---
+++ ColorPicker/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPick_Click(object sender, EventArgs e)
+        {
+            if (rdbRed.Checked == true)
+                ShowPicked("Red\n#" + Color.Red.GetHashCode().ToString(), Color.Red);
+            else if (rdbGreen.Checked == true)
+                ShowPicked("Green\n#" + Color.Green.GetHashCode().ToString(), Color.Green);
+            else if (rdbBlue.Checked == true)
+                ShowPicked("Blue\n#" + Color.Blue.GetHashCode().ToString(), Color.Blue);
+            else if (rdbYellow.Checked == true)
+                ShowPicked("Playing Despacito...", Color.Yellow);
+        }
+
+        private void ShowPicked(string message, Color color)
+        {
+            pbxCanvas.BackColor = color;
+            MessageBox.Show(message);
+        }
+    }
+}
